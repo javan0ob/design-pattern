dbad0ca52f69ae2af4c48d86054902dcba5d1e87
miku <240607462@qq.com>
2021-07-19 23:54:48 +0800

    Update AbstractFactoryTest.java
    uodate

--- src/main/java/com/dp/creationalpattern/abstractfactory/AbstractFactoryTest.java
+++ src/main/java/com/dp/creationalpattern/abstractfactory/AbstractFactoryTest.java
@@ -3,7 +3,7 @@ package com.dp.creationalpattern.abstractfactory;
 /**
  * 抽象工厂产生多个抽象"零件"(一系列相关或相互依赖对象)
  * 抽象工厂由一组工厂方法组成
- * 符合单一职责和开闭原则，无需指定她们具体的类
+ * 符合单一职责和开闭原则
  * java.sql.Connection抽象工厂
  */
 public class AbstractFactoryTest {
