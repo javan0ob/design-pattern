8bb623ded5d24899f99afa3bf55c7bbe81002ef4
rpy <240607462@qq.com>
2021-07-19 23:54:18 +0800

    update

--- src/main/java/com/dp/creationalpattern/abstractfactory/AbstractFactoryTest.java
+++ src/main/java/com/dp/creationalpattern/abstractfactory/AbstractFactoryTest.java
@@ -1,7 +1,11 @@
 package com.dp.creationalpattern.abstractfactory;
 
-//抽象工厂产生多个抽象零件
-//抽象工厂由一组工厂方法组成
+/**
+ * 抽象工厂产生多个抽象"零件"(一系列相关或相互依赖对象)
+ * 抽象工厂由一组工厂方法组成
+ * 符合单一职责和开闭原则，无需指定她们具体的类
+ * java.sql.Connection抽象工厂
+ */
 public class AbstractFactoryTest {
     public static void main(String[] args) {
         ComputerFactory computerFactory = new HuaweiComputerFactory();
